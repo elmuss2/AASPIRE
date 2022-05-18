5ced8929a90ffb0f2c440b6f8218f559494c5065
Nathan Stoltenberg <nds4@pdx.edu>
2022-05-17 17:08:02 -0700

    make db easy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,15 +80,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'aaspiredb',
-        'USER': 'root',
-        'PASSWORD': 'example',
-        'HOST': '127.0.0.1',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
